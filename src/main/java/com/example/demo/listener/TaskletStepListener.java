125be4f84b4ab81e1ce8275f7645183bffa83317
keisukehasegawa0918 <chiro9689@gmail.com>
2022-11-21 16:53:42 +0900

    StepListener作成

--- src/main/java/com/example/demo/listener/TaskletStepListener.java
+++ src/main/java/com/example/demo/listener/TaskletStepListener.java
@@ -1,5 +1,28 @@
 package com.example.demo.listener;
 
-public class TaskletStepListener {
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.StepExecutionListener;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+public class TaskletStepListener implements StepExecutionListener {
+	
+	@Override
+	public void beforeStep(StepExecution stepExecution) {
+		
+		// Do nothing
+	}
+
+	@Override
+	public ExitStatus afterStep(StepExecution stepExecution) {
+		
+		log.info(stepExecution.toString());
+		
+		return stepExecution.getExitStatus();
+	}
 
 }
